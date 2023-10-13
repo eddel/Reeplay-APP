07259efab6f11aff99a7087a398370cddb77aa43
PHP AND CODE MASTERS <99367077+officialphp@users.noreply.github.com>
2023-10-13 14:01:17 +0100

    Update Register.js

--- screens/Register/Register.js
+++ screens/Register/Register.js
@@ -1,4 +1,4 @@
-import React, { useState } from "react";
+import React, { useState, useEffect } from "react";
 import {
   StyleSheet,
   Text,
@@ -21,18 +21,11 @@ import NumField from "./components/NumField";
 import { Ionicons } from "@expo/vector-icons";
 import { LinearGradient } from "expo-linear-gradient";
 import { KeyboardAvoidingView } from "react-native";
-
-// Import the "Pin" component
-import Pin from "./components/pin";
-
 export default function Register({ navigation }) {
   const [name, setname] = useState();
   const [email, setEmail] = useState();
   const [password, setPassword] = useState();
   const [isFocusedE, setIsFocusedE] = useState(false);
-
-  // Add the showPin state variable and togglePin function
-  const [showPin, setShowPin] = useState(false);
 
   const handleFocusE = () => {
     setIsFocusedE(true);
@@ -41,17 +34,31 @@ export default function Register({ navigation }) {
   const handleBlurE = () => {
     setIsFocusedE(false);
   };
+  const [isFocusedP, setIsFocusedP] = useState(false);
 
-  // Other functions and state variables...
-
-  // Function to toggle the display of the "Pin" component
-  const togglePin = () => {
-    setShowPin(!showPin);
+  const handleFocusP = () => {
+    setIsFocusedP(true);
   };
+
+  const handleBlurP = () => {
+    setIsFocusedP(false);
+  };
+  const [isFocusedF, setIsFocusedF] = useState(false);
+
+  const handleFocusF = () => {
+    setIsFocusedF(true);
+  };
+
+  const handleBlurF = () => {
+    setIsFocusedF(false);
+  };
+
+  const [PromotionToggle, setPromotionToggle] = useState();
 
   return (
     <View style={styles.container}>
       <StatusBar style="auto" />
+
       <ImageBackground
         source={require("../../assets/ONBOARDING.png")}
         style={styles.BackGround}
@@ -61,22 +68,139 @@ export default function Register({ navigation }) {
           style={styles.background}
         />
         <ScrollView>
-          <View style={{ paddingTop: StatusBar.currentHeight }}>
-            <Logo />
-          </View>
+          <Logo />
+
           <KeyboardAvoidingView style={styles.FieldsWrapper}>
-            {/* Your existing code for text fields, buttons, and other components */}
+            <TextField
+              Holder="Full Names"
+              handleFocus={handleFocusF}
+              handleBlur={handleBlurF}
+              isFocused={isFocusedF}
+              value={name}
+              onChangeText={(t) => setname(t)}
+            />
+            <NumField />
+            <TextField
+              Holder="Email"
+              handleFocus={handleFocusE}
+              handleBlur={handleBlurE}
+              isFocused={isFocusedE}
+              value={email}
+              onChangeText={(t) => setEmail(t)}
+            />
+            <PasswordField
+              password="Password"
+              handleFocus={handleFocusP}
+              handleBlur={handleBlurP}
+              isFocused={isFocusedP}
+              value={password}
+              onChangeText={(t) => setPassword(t)}
+            />
+            <View style={styles.PromotionWrapper}>
+              <TouchableOpacity
+                onPress={() => setPromotionToggle(!PromotionToggle)}
+                style={[
+                  styles.PromotionToggleView,
+                  {
+                    backgroundColor:
+                      PromotionToggle == 1 ? "#fff" : "transparent",
+                  },
+                ]}
+              >
+                {PromotionToggle == 1 ? (
+                  <Ionicons name="md-checkmark-sharp" size={12} color="red" />
+                ) : null}
+              </TouchableOpacity>
+              <Text style={styles.PromotionText}>
+                Receive promotion and notification emails
+              </Text>
+            </View>
             <Btn
               color="#FF1313"
               Title="Create a New Account"
-              navigation={togglePin} // Call togglePin function
+              navigation={() => navigation.navigate("VerifyEmail")}
             />
+
+            <TouchableOpacity onPress={() => navigation.navigate("Login")}>
+              <Text style={[styles.AccountText, { marginVertical: 12 }]}>
+                Login
+              </Text>
+            </TouchableOpacity>
+            <Text style={styles.ForgotPassText}>
+              if you already have a Reeplay Account
+            </Text>
           </KeyboardAvoidingView>
         </ScrollView>
-        {showPin && <Pin navigation={navigation} />}
       </ImageBackground>
     </View>
   );
 }
 
-// Your existing styles...
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: "#000000",
+  },
+  BackGround: {
+    height: hp("100%"),
+    width: wp("100%"),
+  },
+  BgLayer: {
+    height: hp("100%"),
+    width: wp("100%"),
+    backgroundColor: "#000000",
+    opacity: 0.7,
+  },
+  background: {
+    position: "absolute",
+    right: 0,
+    bottom: 0,
+    height: 900,
+    left: 0,
+    flexGrow: 1,
+  },
+  FieldsWrapper: {
+    height: hp("60%"),
+    width: wp("90%"),
+    backgroundColor: "#0B0B0B",
+    alignSelf: "center",
+    borderRadius: 20,
+    padding: 10,
+    marginBottom: 200,
+  },
+  AccountText: {
+    fontSize: rf(13),
+
+    color: "#FF1313",
+    textAlign: "center",
+    fontFamily: "robotobold",
+  },
+  ForgotPassText: {
+    fontSize: rf(10),
+    fontWeight: "400",
+    color: "#E5E5E5",
+    textAlign: "center",
+    fontFamily: "roboto",
+  },
+  PromotionToggleView: {
+    height: 15,
+    width: 15,
+    borderWidth: 1,
+    borderColor: "#C4C4C4",
+    marginRight: 10,
+    justifyContent: "center",
+    alignItems: "center",
+    borderRadius: 3,
+  },
+  PromotionText: {
+    color: "#C4C4C4",
+    fontSize: rf(9),
+    fontWeight: "400",
+    fontFamily: "roboto",
+  },
+  PromotionWrapper: {
+    flexDirection: "row",
+    alignSelf: "center",
+    marginVertical: hp("2%"),
+  },
+});
